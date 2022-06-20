2a183b47d7560a68298f05d9e1f7ee811736d5b0
GaraninNV <nikolas-ua@mail.ru>
2022-06-21 00:27:28 +0300

    2. Сокобан. Тестовое задание. [#75283 #314296] _1

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,7 +3,7 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         for (int i = 0; i < board.length; i++) {
-           if (board[i][i] == 1 && horizonWin(board, i) || verticalWin(board, i)) {
+           if (board[i][i] == 1 && (horizonWin(board, i) || verticalWin(board, i))) {
                 return true;
            }
         }
